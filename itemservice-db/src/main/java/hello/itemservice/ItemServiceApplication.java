805b8b93ee0a322eb0ba51b5ed934bbe4d284e9c
jinju1123 <doc9733@gmail.com>
2023-09-26 17:43:04 +0900

    42일차 - JPA 완료

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -17,7 +17,8 @@ import javax.sql.DataSource;
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
 //@Import(JdbcTemplateV3Config.class)
-@Import(MyBatisConfig.class)
+//@Import(MyBatisConfig.class)
+@Import(JpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
@@ -31,15 +32,4 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
-//	@Bean
-//	@Profile("test")
-//	public DataSource dataSource() {
-//		log.info("메모리 데이터베이스 초기화");
-//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//		dataSource.setDriverClassName("org.h2.Driver");
-//		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
-//		dataSource.setUsername("sa");
-//		dataSource.setPassword("");
-//		return dataSource;
-//	}
 }
